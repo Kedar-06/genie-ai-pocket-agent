4a669f5b67db415de23342593670ac8b434635b5
Kedar Ghadyalji <kedarghadyalji@gmail.com>
2025-09-30 23:29:36 +0530

    Create Clerk Auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,18 @@
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Stack.Screen name="index" />{" "}
-    </Stack>
+    <ClerkProvider tokenCache={tokenCache}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="index" />
+      </Stack>
+    </ClerkProvider>
   );
 }

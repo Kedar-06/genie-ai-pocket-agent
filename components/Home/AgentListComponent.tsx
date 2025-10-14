24b15db9d055986c0c1471ec86c6147a96bcebae
Kedar Ghadyalji <kedarghadyalji@gmail.com>
2025-10-14 15:35:46 +0530

    Create ChatUI

--- components/Home/AgentListComponent.tsx
+++ components/Home/AgentListComponent.tsx
@@ -1,10 +1,12 @@
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import React from "react";
 import { Agents } from "../../shared/AgentList";
 import AgentCard from "./AgentCard";
 import NonFeaturedAgentCard from "./NonFeaturedAgentCard";
+import { useRouter } from "expo-router";
 
 export default function AgentListComponent({ isFeatured }: any) {
+  const router = useRouter();
   return (
     <View>
       <FlatList
@@ -13,18 +15,29 @@ export default function AgentListComponent({ isFeatured }: any) {
         // @ts-ignore
         renderItem={({ item, index }) =>
           item.featured === isFeatured && (
-            <View
+            <TouchableOpacity
               style={{
                 flex: 1,
                 padding: 5,
               }}
+              onPress={() =>
+                router.push({
+                  pathname: "/chat",
+                  params: {
+                    agentName: item.name,
+                    initialText: item.initialText,
+                    agentPrompt: item.prompt,
+                    agentId: item.id,
+                  },
+                })
+              }
             >
               {item.featured ? (
                 <AgentCard agent={item} key={index} />
               ) : (
                 <NonFeaturedAgentCard agent={item} key={index} />
               )}
-            </View>
+            </TouchableOpacity>
           )
         }
       />
